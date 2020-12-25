0faba87f736bae95e4f581d7324a90440d3344c4
Yuchen Xu <yx439@cornell.edu>
2020-12-25 15:34:46 -0500

    projtest

--- R/projTest.R
+++ R/projTest.R
@@ -6,6 +6,7 @@
 #' @param d Matrix dimension
 #' @param param.out Logical. Whether the parameters need to be included in output
 #' @param eps The threshold of eigenvalues when compute general inverse of covariance matrices
+#' @param refMat The list of reference matrices with the same eigenvectors, default is to use the estimated \code{A}
 #'
 #' @return The p-value or a list of test information.
 #' \itemize{
@@ -13,10 +14,13 @@
 #' \item df Degrees of freedom for chi-squared distribution.
 #' \item pvalue P-value.
 #' }
+#' @importFrom MASS ginv
 #' @export
 #'
+#' @description Only valid for nonsingular covariance matrices \code{covList}.
+#'
 #' @examples projTest(countryCoeff, countryCovar, cn = sqrt(112), eps = 112^(-1/3))
-projTest = function(A, covList = list(), cn, eps, d = ncol(A[[1]]), param.out = FALSE){
+projTest = function(A, covList = list(), refMat = A, cn, eps, d = ncol(A[[1]]), param.out = FALSE){
 
   p = 2
 
@@ -28,8 +32,13 @@ projTest = function(A, covList = list(), cn, eps, d = ncol(A[[1]]), param.out = 
     cov2 = covList[[2]]
   }
 
-  C = A[[1]]
-  D = A[[2]]
+  if (length(refMat) == 1) {
+    refMat = list(refMat[[1]], refMat[[1]])
+  }
+
+  X = A[[1]]; Y = A[[2]]
+
+  C = refMat[[1]]; D = refMat[[2]]
 
   C.temp = diag(d)
   D.temp = diag(d)
@@ -43,14 +52,13 @@ projTest = function(A, covList = list(), cn, eps, d = ncol(A[[1]]), param.out = 
     SP.C = cbind(SP.C, as.vector(C.temp))
     SP.D = cbind(SP.D, as.vector(D.temp))
   }
-  tSVD1 = truncateSVD(cov1, eps); tSVD2 = truncateSVD(cov2, eps)
-  inner1 = truncateSVD(crossprod(SP.D, tSVD1$ginv %*% SP.D), eps)
-  inner2 = truncateSVD(crossprod(SP.C, tSVD2$ginv %*% SP.C), eps)
-  Q1 = tSVD1$ginv - tSVD1$ginv %*% SP.D %*% inner1$ginv %*% t(SP.D) %*% tSVD1$ginv
-  Q2 = tSVD2$ginv - tSVD2$ginv %*% SP.C %*% inner2$ginv %*% t(SP.C) %*% tSVD2$ginv
+  cov1.r = ginv(cov1); cov2.r = ginv(cov2)
+  inner1 = ginv(crossprod(SP.D, cov1.r %*% SP.D)); inner2 = ginv(crossprod(SP.C, cov2.r %*% SP.C))
+  Q1 = cov1.r - cov1.r %*% SP.D %*% inner1 %*% t(SP.D) %*% cov1.r
+  Q2 = cov2.r - cov2.r %*% SP.C %*% inner2 %*% t(SP.C) %*% cov2.r
 
-  r = tSVD1$r + tSVD2$r - inner1$r - inner2$r
-  testVal = crossprod(as.double(C), Q1 %*% as.double(C)) + crossprod(as.double(D), Q2 %*% as.double(D))
+  r = 2*d*(d-1)
+  testVal = crossprod(as.double(X), Q1 %*% as.double(X)) + crossprod(as.double(Y), Q2 %*% as.double(Y))
   testVal = testVal * cn^2
 
   if (param.out) {
