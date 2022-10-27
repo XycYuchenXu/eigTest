e247e29901b367b471b3658fd5fcc945cd21e812
XycYuchenXu <XycXu1015@gmail.com>
2022-10-27 12:35:24 -0400

    code script

--- code/partialTest.R
+++ code/partialTest.R
@@ -6,11 +6,10 @@ library(tidyverse)
 #library(tikzDevice)
 
 set.seed(2020)
-samples = 20
+samples = 200
 d = 4
 p = 8
 k = 2
-#SNRS = c(100); n = sqrt(c(100,500))
 SNRS = c(1000, 100, 10)
 n = sqrt(c(100, 1000, 10000))
 means_p = generateMeans(d, p, k, snr = SNRS, control.g = TRUE)
@@ -33,13 +32,23 @@ data_p = foreach(est_list = simulated_p, .inorder = F, .combine = bind_rows,
 
                    p_vector_partial = array(NA, dim = c(2, d-k+1))
                    for (kk in k:d) {
-                     if (kk < d) {Qk = expmPartSchur(mu.bar, kk, warmup = T)}
+                     if (kk < d) {
+                       Qk = expmPartSchur(mu.bar, kk, warmup = T)
+                       B = array(0, c(p,kk,kk))
+                       for (i in 1:p) {
+                         Ai = tcrossprod(crossprod(Qk, mu.bar[i,,]), t(Qk))
+                         B[i,,] = Ai[1:kk, 1:kk]
+                       }
+                       V = JDTE(B)
+                     } else {
+                       Qk = NULL; V = JDTE(mu.bar)
+                     }
                      p_vector_partial[1, kk-k+1] = partialTest(mu.bar, cn = CovRate,
-                                                               cov.arr = cov.bar, Q = Qk,
+                                                               cov.arr = cov.bar, Q = Qk, V = V,
                                                                k = kk, testType = 'chi')
                      print(kk)
                      p_vector_partial[2, kk-k+1] = partialTest(mu.bar, cn = CovRate,
-                                                               cov.arr = cov.bar, Q = Qk,
+                                                               cov.arr = cov.bar, Q = Qk, V = V,
                                                                k = kk, testType = 'gam')
                    }
                    dimnames(p_vector_partial) = list(c('Chi', 'Gam'), paste('K =', k:d))
@@ -58,7 +67,7 @@ save(data_p, file = 'output/partTest.RData')
 breaks = seq(0,1,0.05); binwidth = 0.05
 
 #tikz(file = "output/Plots/PvaluePartial.tikz", standAlone=F, width = 6, height = 4.5)
-ggplot(data_p) +
+ggplot(data_p %>% filter(K == 'K = 2')) +
   geom_histogram(aes(x = pvalue, y = ..density..*binwidth, fill = SNR),
                  breaks = breaks,
                  position = position_dodge()) + theme_bw()+
